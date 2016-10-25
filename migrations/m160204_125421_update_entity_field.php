38395866f218f1f0998b20751c73d41347104178
execut <sexecut@gmail.com>
2016-10-25 13:50:24 +0300

    Postgresql adaptation in migrations

--- migrations/m160204_125421_update_entity_field.php
+++ migrations/m160204_125421_update_entity_field.php
@@ -5,15 +5,39 @@ use yii\db\Schema;
 
 class m160204_125421_update_entity_field extends Migration
 {
-    public function up()
+    public function safeUp()
     {
-        $this->alterColumn('seo', 'entity', Schema::TYPE_STRING . '(255) NOT NULL');
+        $this->alterColumn('seo', 'entity', $this->string());
+        $this->execute(<<<SQL
+ALTER TABLE seo ALTER COLUMN entity DROP DEFAULT;
+SQL
+);
+        if ($this->db->driverName === 'pgsql') {
+            $this->execute(<<<SQL
+DROP TYPE seo_entity;
+SQL
+            );
+        }
+
+        $this->execute(<<<SQL
+ALTER TABLE seo ALTER COLUMN entity SET DEFAULT 'page'
+SQL
+        );
     }
 
-    public function down()
+    public function safeDown()
     {
-        echo "m160204_125421_update_entity_field cannot be reverted.\n";
-
-        return false;
+        if ($this->db->driverName === 'pgsql') {
+            $this->execute(<<<SQL
+CREATE TYPE seo_entity AS ENUM ('page');
+SQL
+            );
+            $this->dropColumn('seo', 'entity');
+            $entityDefinition = "seo_entity NOT NULL DEFAULT 'page'";
+            $this->addColumn('seo', 'entity', $entityDefinition);
+        } else {
+            $entityDefinition = "ENUM('page') NOT NULL DEFAULT 'page'";
+            $this->alterColumn('seo', 'entity', $entityDefinition);
+        }
     }
 }
